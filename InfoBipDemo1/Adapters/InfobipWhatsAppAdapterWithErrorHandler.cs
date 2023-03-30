dad9b256647c638d8a425d96c56c83e3306b8c16
egunesen <egunesen@ENIS001>
2023-03-30 17:40:32 +0300

    Add project files.

---
+++ InfoBipDemo1/Adapters/InfobipWhatsAppAdapterWithErrorHandler.cs
@@ -0,0 +1,24 @@
+﻿using Bot.Builder.Community.Adapters.Infobip.WhatsApp;
+using Microsoft.Extensions.Logging;
+
+namespace InfoBipDemo1.Adapters
+{
+    public class InfobipWhatsAppAdapterWithErrorHandler : InfobipWhatsAppAdapter
+    {
+        public InfobipWhatsAppAdapterWithErrorHandler(InfobipWhatsAppAdapterOptions infobipWhatsAppOptions, IInfobipWhatsAppClient infobipWhatsAppClient, ILogger<InfobipWhatsAppAdapterWithErrorHandler> logger)
+            : base(infobipWhatsAppOptions, infobipWhatsAppClient, logger)
+        {
+            OnTurnError = async (turnContext, exception) =>
+            {
+                OnTurnError = async (turnContext, exception) =>
+                {
+                    // Log any leaked exception from the application.
+                    logger.LogError($"Exception caught : {exception.Message}");
+
+                    // Send a catch-all apology to the user.
+                    await turnContext.SendActivityAsync("Sorry, it looks like something went wrong.");
+                };
+            };
+        }
+    }
+}
---
+++ InfoBipDemo1/Controllers/InfobipController.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Core;
+using Bot.Builder.Community.Adapters.Infobip.WhatsApp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using System.Threading.Tasks;
+
+namespace InfoBipDemo1.Controllers
+{
+    [Route("api/infobip/whatsapp")]
+    [ApiController]
+    public class InfobipController : ControllerBase
+    {
+        private readonly InfobipWhatsAppAdapter Adapter;
+        private readonly IBot Bot;
+
+        public InfobipController(InfobipWhatsAppAdapter adapter, IBot bot)
+        {
+            Adapter = adapter;
+            Bot = bot;
+        }
+
+        [HttpPost]
+        public async Task PostAsync()
+        {
+            // Delegate the processing of the HTTP POST to the adapter.
+            // The adapter will invoke the bot.
+            await Adapter.ProcessAsync(Request, Response, Bot);
+        }
+    }
+}
